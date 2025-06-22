eb8db170162a1d2842fd9760919e3fdb2ed82ba5
ifti2021331087 <ishmamiftinoor3@gmail.com>
2025-06-22 13:52:26 +0600

    new commit

--- js/extra.js
+++ js/extra.js
@@ -10,3 +10,8 @@ document.getElementById('history-tab').addEventListener('click',function(){
     document.getElementById('history-tab').classList.add('active');
 });
 
+document.getElementById('close-btn').addEventListener('click',function(){
+    showExceptID();
+    document.getElementById('card-container').classList.remove('hidden');
+    document.getElementById('donation-tab').classList.add('active');
+});
