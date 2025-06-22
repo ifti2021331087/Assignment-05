b08f1cfa27cf7347892dae6b0ee6907a50550c6e
ifti2021331087 <ishmamiftinoor3@gmail.com>
2025-06-22 08:36:09 +0600

    new changes

--- js/donation.js
+++ js/donation.js
@@ -18,3 +18,43 @@ document.getElementById('donateN').addEventListener('click',function(event){
         alert('Invalid Input');
     }
 });
+
+document.getElementById('donateF').addEventListener('click',function(event){
+    event.preventDefault();
+    const mainBalance=getTextValue('main-account');
+    const inputBalance=getInputValue('inputF');
+    const showBalance=getTextValue('showF');
+    if(inputBalance>0 && inputBalance<=mainBalance){
+        
+        const newBalance=showBalance+inputBalance;
+        const newMainBalance=mainBalance-inputBalance;
+        document.getElementById('showF').innerText=newBalance;
+        document.getElementById('main-account').innerText=newMainBalance;
+    }
+    else if(mainBalance<=0){
+        alert('Your Account Balance Is Not Enough');
+    }
+    else{
+        alert('Invalid Input');
+    }
+});
+
+document.getElementById('donateQ').addEventListener('click',function(event){
+    event.preventDefault();
+    const mainBalance=getTextValue('main-account');
+    const inputBalance=getInputValue('inputQ');
+    const showBalance=getTextValue('showQ');
+    if(inputBalance>0 && inputBalance<=mainBalance){
+        
+        const newBalance=showBalance+inputBalance;
+        const newMainBalance=mainBalance-inputBalance;
+        document.getElementById('showQ').innerText=newBalance;
+        document.getElementById('main-account').innerText=newMainBalance;
+    }
+    else if(mainBalance<=0){
+        alert('Your Account Balance Is Not Enough');
+    }
+    else{
+        alert('Invalid Input');
+    }
+});
